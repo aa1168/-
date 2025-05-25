0fb019ebb9ba155bb8a8a63676fb9d4ceeb435ff
aa1168 <linx63321@gmail.com>
2025-05-25 22:33:12 +0800

    Add files via upload

---
+++ static/js/swiper.js
@@ -0,0 +1,15 @@
+﻿
+$(document).ready(function () {
+  var swiper = {
+    init: function () {
+      new Swiper('.swiper-container', {
+        loop: true,
+        autoplay: 5000,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        autoplayDisableOnInteraction: false
+      });
+    }
+  };
+  swiper.init();
+});
